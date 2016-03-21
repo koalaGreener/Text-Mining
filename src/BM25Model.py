d03d2517d704c0808838d523fd71e0212126ebc2
HUANGWEIJIE <weijie.huang@ucl.ac.uk>
2016-03-21 16:17:29 +0000

    Added the formatted output.

--- src/BM25Model.py
+++ src/BM25Model.py
@@ -1,11 +1,14 @@
+import operator
 from math import log
+
 
 def func(qi, document_num_name):
     nqi = 0
     for item in document_num_name:
-        if((qi) in document_num_name[item]):
+        if ((qi) in document_num_name[item]):
             nqi += 1
     return nqi
+
 
 def readTheFile(document_name, query_name):
     b = 0.75
@@ -28,7 +31,6 @@ def readTheFile(document_name, query_name):
                 temp.append(int(line[i].split(":")[0]))
             query_id[int(line[0])] = temp
 
-
     document_only_num_list = []
     document_length = {}
     document_num_name = {}
@@ -49,31 +51,25 @@ def readTheFile(document_name, query_name):
             document_length[name] = length_of_document
             document_num_name[name] = document_only_num_map
 
-    #print(document_num_name)
     # avgdl calculated
     sum = 0
     for every in document_only_num_list:
         sum += int(every.split(":")[1])
     avgdl = 1.0 * sum / N
-    #print(avgdl)
+    # print(avgdl)
 
-
-
-    print("start nqi")
-
-    #nqi list
+    # nqi list
     nqi_list = {}
     for query_item in query_id:
         qi_list = query_id.get(query_item)
         for qi_value in qi_list:
             nqi_list[qi_value] = func(qi_value, document_num_name)
 
-    print("end nqi")
     for query_item in query_id:
         # index 保存query中所出现的 term
         qi_list = query_id.get(query_item)
-        #print(qi_list)
-        count = 0
+        sorted_map = {}
+        ranking = 1
         for document_item in document_id:
             score = 0.0
 
@@ -81,8 +77,7 @@ def readTheFile(document_name, query_name):
             D = document_length.get(document_item.split(" ")[0])
 
             for qi in qi_list:
-
-                #fqid 计算
+                # fqid 计算
                 fqid = 0
                 fqid_dict = document_num_name.get(document_item)
                 if fqid_dict.get(qi) == None:
@@ -90,14 +85,19 @@ def readTheFile(document_name, query_name):
                 else:
                     fqid = fqid_dict.get(qi)
 
-                #nqi 计算
+                # nqi 计算
                 nqi = nqi_list.get(qi)
 
-                #score 计算
-                score += (log((N - nqi + 0.5) / (nqi + 0.5))) * 1.0 * fqid * (k + 1)/ (fqid + k * (1 - b + (b * D / avgdl)))
-                print(count, query_item, document_item, score)
-            count += 1
+                # score 计算
+                score += (log((N - nqi + 0.5) / (nqi + 0.5))) * 1.0 * fqid * (k + 1) / (fqid + k * (1 - b + (b * D / avgdl)))
+                sorted_map[document_item] = score
+        ranked_result = sorted(sorted_map.items(), key=lambda x: x[1])
+        ranked_result.reverse()
 
+        # output the formatted result
+        for everyitem in ranked_result:
+            print(str(query_item) + " Q0 " + str(everyitem).strip("' ( '").split("',")[0] + " " + str(ranking) + str(everyitem).strip("'( ')").split(",")[1] + " bm25")
+            ranking += 1
 
 '''import the data'''
 if __name__ == '__main__':
