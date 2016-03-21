9856711fee356e4c583bcc0e6cd25056bdf1b6a0
HUANGWEIJIE <weijie.huang@ucl.ac.uk>
2016-03-21 16:24:50 +0000

    Added some extra comments.

--- src/BM25Model.py
+++ src/BM25Model.py
@@ -1,4 +1,3 @@
-import operator
 from math import log
 
 
@@ -11,17 +10,20 @@ def func(qi, document_num_name):
 
 
 def readTheFile(document_name, query_name):
+    # Parameters
     b = 0.75
     k = 1.5
     avgdl = 0.0  # 1374.882219471947
     N = 4848
-    # store the data in this two list
+
+    # store the data in those lists
     query_id = {}
     document_id = []
-    with open(document_name) as infile:
-        for line in infile:
-            document_id.append(line.split(" ")[0])
+    document_only_num_list = []
+    document_length = {}
+    document_num_name = {}
 
+    # read the query
     with open(query_name) as infile:
         for line in infile:
             temp = []
@@ -31,11 +33,10 @@ def readTheFile(document_name, query_name):
                 temp.append(int(line[i].split(":")[0]))
             query_id[int(line[0])] = temp
 
-    document_only_num_list = []
-    document_length = {}
-    document_num_name = {}
+    # read the document
     with open(document_name) as infile:
         for line in infile:
+            document_id.append(line.split(" ")[0])
             name = ""
             document_only_num_map = {}
             length_of_document = 0
@@ -56,48 +57,54 @@ def readTheFile(document_name, query_name):
     for every in document_only_num_list:
         sum += int(every.split(":")[1])
     avgdl = 1.0 * sum / N
-    # print(avgdl)
 
-    # nqi list
+    # nqi list(the number of documents containing qi)
     nqi_list = {}
     for query_item in query_id:
         qi_list = query_id.get(query_item)
         for qi_value in qi_list:
             nqi_list[qi_value] = func(qi_value, document_num_name)
 
+    # Two iterations to calculated the score (first one)
     for query_item in query_id:
-        # index 保存query中所出现的 term
         qi_list = query_id.get(query_item)
         sorted_map = {}
         ranking = 1
+        #(second one)
         for document_item in document_id:
             score = 0.0
 
             # D = the length of the document D in words
             D = document_length.get(document_item.split(" ")[0])
 
+            # calculated each query_term's score and then added them into score
             for qi in qi_list:
-                # fqid 计算
+
+                # fqid calculated
                 fqid = 0
                 fqid_dict = document_num_name.get(document_item)
-                if fqid_dict.get(qi) == None:
+                # if the term doesn't exist, then return 0
+                if fqid_dict.get(qi) is None:
                     fqid = 0
                 else:
                     fqid = fqid_dict.get(qi)
 
-                # nqi 计算
+                # nqi calculated
                 nqi = nqi_list.get(qi)
 
-                # score 计算
-                score += (log((N - nqi + 0.5) / (nqi + 0.5))) * 1.0 * fqid * (k + 1) / (fqid + k * (1 - b + (b * D / avgdl)))
+                # score calculated
+                score += (log((N - nqi + 0.5) / (nqi + 0.5))) * 1.0 * fqid * (k + 1) / (
+                fqid + k * (1 - b + (b * D / avgdl)))
                 sorted_map[document_item] = score
         ranked_result = sorted(sorted_map.items(), key=lambda x: x[1])
         ranked_result.reverse()
 
         # output the formatted result
         for everyitem in ranked_result:
-            print(str(query_item) + " Q0 " + str(everyitem).strip("' ( '").split("',")[0] + " " + str(ranking) + str(everyitem).strip("'( ')").split(",")[1] + " bm25")
+            print(str(query_item) + " Q0 " + str(everyitem).strip("' ( '").split("',")[0] + " " + str(ranking) +
+                  str(everyitem).strip("'( ')").split(",")[1] + " bm25")
             ranking += 1
+
 
 '''import the data'''
 if __name__ == '__main__':
