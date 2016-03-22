6b654ed2f43cc2ce347409f545638a2a59492d7d
HUANGWEIJIE <weijie.huang@ucl.ac.uk>
2016-03-22 19:39:14 +0000

    Latest code.

--- src/BM25Model.py
+++ src/BM25Model.py
@@ -1,4 +1,4 @@
-from math import log
+from math import log, log2
 
 
 def func(qi, document_num_name):
@@ -71,7 +71,7 @@ def readTheFile(document_name, query_name):
     for query_item in query_id:
         qi_list = query_id.get(query_item)
         sorted_map = {}
-        ranking = 1
+        ranking = 0
         #(second one)
         for document_item in document_id:
             score = 0.0
@@ -108,8 +108,170 @@ def readTheFile(document_name, query_name):
             ranking += 1
     return return_value
 
+def readTheFile_idcg(filename):
+    term_rankging_score = []
+    # read the idcg data
+    with open(filename) as infile:
+        for line in infile:
+            temp = {}
+            each = line.split(" ")
+            id = each[0]
+            ranking = each[3]
+            score = each[4]
+            if int(ranking) < 50:
+                term_rankging_score.append(str(id) + " " + str(ranking) + " " + str(score))
+        return term_rankging_score
+
+
+
+
+
+
 '''import the data'''
 if __name__ == '__main__':
-    result = readTheFile("../data/document_term_vectors.dat", "../data/query_term_vectors.dat")
-    for item in result:
-        print(item)
+
+
+    def calculated_DCG_score(input_data):
+        preprocessed_output_dict = {}
+        ranking_score = {}
+        return_dcg_term_value = {}
+        count = 0
+        for data in input_data:
+            id = data.split(" ")[0]
+            ranking = data.split(" ")[1]
+            score = data.split(" ")[2]
+            #print(id,ranking,score)
+            ranking_score[ranking] = score
+            count += 1
+            if count == 50:
+                preprocessed_output_dict[id] = ranking_score
+                ranking_score = {}
+                count = 0
+        count = 0
+        for data in preprocessed_output_dict:
+            # data = 201
+            # K:score
+            dcg_value = {}
+            ranking_score_dict = preprocessed_output_dict.get(data)
+            for i in range(1, 51):
+                temp_dcg_sum = 0.0
+                if i == 1:
+                    dcg =  ranking_score_dict.get(str(i - 1))
+                    temp_dcg_sum += float(dcg)
+                    dcg_value[i] = float(dcg)
+                else:
+                    dcg = temp_dcg_sum + float(ranking_score_dict.get(str(i - 1))) / log2(i)
+                    dcg_value[i] = float(dcg)
+            count += 1
+            #print(data, dcg_value)
+            return_dcg_term_value[data] = dcg_value
+        #print(count)
+        return return_dcg_term_value
+
+
+    # import the data
+    dcg_data = readTheFile("../data/document_term_vectors.dat", "../data/query_term_vectors.dat")
+
+    dcg_term_rankging_score = []
+    value = {}
+    for data in dcg_data:
+        each = data.split(" ")
+        id = each[0]
+        ranking = each[3]
+        score = each[4]
+        if int(ranking) < 50:
+            dcg_term_rankging_score.append(str(id) + " " + str(ranking) + " " + str(score))
+
+    idcg_term_rankging_score = readTheFile_idcg("../data/BM25b0.75_0.res")
+
+    # import the data
+
+
+    dcg_value = calculated_DCG_score(dcg_term_rankging_score)
+    idcg_value = calculated_DCG_score(idcg_term_rankging_score)
+    k_score = {}
+    term_k_score = {}
+    #print((dcg_value))
+    #print((idcg_value))
+
+    #calculate the NDCG
+    for index_term in idcg_value:
+        k_score = {}
+        for index_K in range(1, 51):
+            ndcg = 1.0 * dcg_value[index_term][index_K] / idcg_value[index_term][index_K]
+            k_score[index_K] = ndcg
+        term_k_score[index_term] = k_score
+
+
+    print(term_k_score)
+
+'''
+    temp1 = 0.0
+    temp5 = 0.0
+    for i in term_k_score:
+        print("----")
+        print("topic:" + str(i))
+        print("K" + "  " + "NDCG@K")
+        print("1" + " " + str(term_k_score[i][1]))
+        print("5" + " " + str(term_k_score[i][5]))
+        print("10" + " " + str(term_k_score[i][10]))
+        print("20" + " " + str(term_k_score[i][20]))
+        temp1 += term_k_score[i][1]
+        temp5 += term_k_score[i][5]
+
+    print(temp1 / 48)
+    print(temp5 / 48)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def calculated_DCG_score(input_data):
+        preprocessed_output_dict = {}
+        ranking_score = {}
+        return_dcg_term_value = {}
+        count = 0
+        for data in input_data:
+            id = data.split(" ")[0]
+            ranking = data.split(" ")[1]
+            score = data.split(" ")[2]
+            #print(id,ranking,score)
+            ranking_score[ranking] = score
+            count += 1
+            if count == 50:
+                preprocessed_output_dict[id] = ranking_score
+                ranking_score = {}
+                count = 0
+        count = 0
+        for data in preprocessed_output_dict:
+            # data = 201
+            # K:score
+            dcg_value = {}
+            ranking_score_dict = preprocessed_output_dict.get(data)
+            for i in range(1, 51):
+                temp_dcg_sum = 0.0
+                if i == 1:
+                    dcg =  ranking_score_dict.get(str(i - 1))
+                    temp_dcg_sum += float(dcg)
+                    dcg_value[i] = float(dcg)
+                else:
+                    dcg = temp_dcg_sum + float(ranking_score_dict.get(str(i - 1))) / log2(i)
+                    dcg_value[i] = float(dcg)
+            count += 1
+            #print(data, dcg_value)
+            return_dcg_term_value[data] = dcg_value
+        #print(count)
+        return return_dcg_term_value
+
+
+
+'''
